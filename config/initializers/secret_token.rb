d0d79af48e7c8c8eb90f903acc80c6ebc5932040
yabuki-ryosuke <yabuki-ryosuke@kayac.com>
2013-06-16 16:49:24 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FhotoBook::Application.config.secret_key_base = 'f242ed3991d13f858dcc5941003b946e2736e5c436e1fd053109d8acac50b688bd0a9ef11ad7faf43acd6699674d4cd3b8f6aa073b45965d3a9e41c124abe7f3'
